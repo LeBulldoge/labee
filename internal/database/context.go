88c5b4c0e8050f844fc6755f589ac8213e6f8d01
Alex Bulldog <alexey13031@gmail.com>
2023-04-20 23:50:28 +0900

    Added database/context.go

---
+++ internal/database/context.go
@@ -0,0 +1,23 @@
+package database
+
+import (
+	"context"
+	"fmt"
+)
+
+type dbContextKeyType string
+
+const dbContextKey dbContextKeyType = "db"
+
+func WithDatabase(ctx context.Context, db *DB) context.Context {
+	return context.WithValue(ctx, dbContextKey, db)
+}
+
+func FromContext(ctx context.Context) (*DB, error) {
+	db, ok := ctx.Value(dbContextKey).(*DB)
+	if !ok {
+		return nil, fmt.Errorf("couldn't parse database from context")
+	}
+
+	return db, nil
+}
--- internal/database/database.go
+++ internal/database/database.go
@@ -3,7 +3,6 @@ package database
 import (
 	"context"
 	"errors"
-	"fmt"
 	"path/filepath"
 
 	"github.com/LeBulldoge/labee/internal/database/schema"
@@ -70,23 +69,6 @@ func (m *DB) Close() error {
 	}
 
 	return m.db.Close()
-}
-
-type dbContextKeyType string
-
-const dbContextKey dbContextKeyType = "db"
-
-func WithDatabase(ctx context.Context, db *DB) context.Context {
-	return context.WithValue(ctx, dbContextKey, db)
-}
-
-func FromContext(ctx context.Context) (*DB, error) {
-	db, ok := ctx.Value(dbContextKey).(*DB)
-	if !ok {
-		return nil, fmt.Errorf("couldn't parse database from context")
-	}
-
-	return db, nil
 }
 
 func tx(db *sqlx.DB, ctx context.Context, f func(context.Context, *sqlx.Tx) error) error {
