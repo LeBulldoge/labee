0da96df6529d9ffec07d741c62b8586b328b98a2
Alex Bulldog <alexey13031@gmail.com>
2023-04-21 23:30:01 +0900

    Moved 'add' action into a separate function

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -51,7 +51,7 @@ func Run() {
 			{
 				Name:      "add",
 				Usage:     "Attach labels to files",
-				ArgsUsage: "[subcommand]",
+				ArgsUsage: "[FILE...]",
 				Aliases:   []string{"a"},
 				Flags: []cli.Flag{
 					flagQuiet,
@@ -61,57 +61,7 @@ func Run() {
 						Usage:   "Add comma separated labels to the file [-l \"labelA, labelB\"]. Creates labels if they don't exist",
 					},
 				},
-				Action: func(ctx *cli.Context) error {
-					args := ctx.Args().Slice()
-					if pipeArgsAvailable() {
-						args = append(args, readPipeArgs()...)
-					}
-					if len(args) == 0 {
-						return ErrNoArgs
-					}
-
-					db, err := database.FromContext(ctx.Context)
-					if err != nil {
-						return err
-					}
-
-					labelNames := ctx.StringSlice("labels")
-
-					var absPaths []string
-					for _, v := range args {
-						_, err := os.Stat(v)
-						if err != nil {
-							return fmt.Errorf("file %s does not exist", v)
-						}
-
-						path, err := filepath.Abs(v)
-						if err != nil {
-							return err
-						}
-
-						absPaths = append(absPaths, path)
-					}
-
-					err = db.AddFilesAndLinks(absPaths, labelNames)
-					if err != nil {
-						return err
-					}
-
-					if quiet {
-						return nil
-					}
-
-					for _, path := range absPaths {
-						labels, err := db.GetFileLabels(path)
-						if err != nil {
-							return err
-						}
-						fmt.Println("New file added:")
-						printFileInfo(path, labels)
-					}
-
-					return nil
-				},
+				Action: addLink,
 			},
 			{
 				Name:      "remove",
@@ -140,4 +90,56 @@ func Run() {
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func addLink(ctx *cli.Context) error {
+	args := ctx.Args().Slice()
+	if pipeArgsAvailable() {
+		args = append(args, readPipeArgs()...)
+	}
+	if len(args) == 0 {
+		return ErrNoArgs
+	}
+
+	db, err := database.FromContext(ctx.Context)
+	if err != nil {
+		return err
+	}
+
+	labelNames := ctx.StringSlice("labels")
+
+	var absPaths []string
+	for _, v := range args {
+		_, err := os.Stat(v)
+		if err != nil {
+			return fmt.Errorf("file %s does not exist", v)
+		}
+
+		path, err := filepath.Abs(v)
+		if err != nil {
+			return err
+		}
+
+		absPaths = append(absPaths, path)
+	}
+
+	err = db.AddFilesAndLinks(absPaths, labelNames)
+	if err != nil {
+		return err
+	}
+
+	if quiet {
+		return nil
+	}
+
+	for _, path := range absPaths {
+		labels, err := db.GetFileLabels(path)
+		if err != nil {
+			return err
+		}
+		fmt.Println("New file added:")
+		printFileInfo(path, labels)
+	}
+
+	return nil
 }
